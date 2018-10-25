ef992bca9593833a856f106476a7830e8ec27389
Daniel Parsons <danielparsons3133382@gmail.com>
2018-10-26 09:36:50 +1100

    End of Section 13

---
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { deleteEvent } from '../eventActions';
+import EventList from '../EventList/EventList';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import EventActivity from '../EventActivity/EventActivity';
+
+const mapState = state => ({
+  events: state.events,
+  loading: state.async.loading
+});
+
+const actions = {
+  deleteEvent
+};
+
+class EventDashboard extends Component {
+  handleDeleteEvent = eventId => () => {
+    this.props.deleteEvent(eventId);
+  };
+
+  render() {
+    const { events, loading } = this.props;
+    if (loading) return <LoadingComponent inverted={true} />;
+
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList deleteEvent={this.handleDeleteEvent} events={events} />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <EventActivity />
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default connect(mapState, actions)(EventDashboard);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr'
+import 'semantic-ui-css/semantic.min.css';
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
+import { loadEvents } from './features/event/eventActions'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+store.dispatch(loadEvents())
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop>
+          <ReduxToastr
+            position='bottom-right'
+            transitionIn='fadeIn'
+            transitionOut='fadeOut'
+          />
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
+registerServiceWorker();
